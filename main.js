83bde1cda5cfb33f05e1d45569dcaaec8f791a31
ivantarabanovych <vanjokcitet334@gmail.com>
2024-08-07 00:44:13 +0300

    q3

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
-function greatMessage(){
-    console.log('You have jackpot!');
+var varValue = 'var Value';
+let letValue = 'let Value';
+const constValue = 'const Value';
+
+console.log(varValue);
+console.log(letValue);
+console.log(constValue);
+
+function functionalArea() {
+  var varFunc = 'varFunc';
+  let letFunc = 'letFunc';
+  const constFunc = 'constFunc';
+
+  console.log(varFunc);
+  console.log(letFunc);
+  console.log(constFunc);
+
+  varFunc = 'change Func var';
+  letFunc = 'change Func let';
+
+  console.log(varFunc);
+  console.log(letFunc);
+
+  if (true) {
+
+    var varBlock = 'var Block';
+    let letBlock = 'let Block';
+    const constBlock = 'const Block';
+
+    console.log(varBlock);
+    console.log(letBlock);
+    console.log(constBlock);
+    
+    var varBlock = 'var Block change';
+    letBlock = 'let Block change';
+   
+
+    console.log(letBlock);
+  }
+  
 }
 
-const badMessage = function(){
-    console.log('You lose all money!');
-}
-
-greatMessage();
-badMessage();
+functionalArea();
+console.log(varValue);
+console.log(letValue);
+console.log(constValue);
